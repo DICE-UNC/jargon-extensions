d79b39d1c835e900b33b3ba1f3342be83855d159
rskarbez <skarbez@gmail.com>
2015-05-13 19:05:50 -0400

    #7 metadata templates pre-alpha complete

--- metadata-templates/src/main/java/org/irods/jargon/metadatatemplate/JargonMetadataTemplateConstants.java
+++ metadata-templates/src/main/java/org/irods/jargon/metadatatemplate/JargonMetadataTemplateConstants.java
@@ -3,4 +3,5 @@ package org.irods.jargon.metadatatemplate;
 public class JargonMetadataTemplateConstants {
 	public static final String MD_TEMPLATE_UNIT = "iRODS:MDTemplate";
 	public static final String MD_ELEMENT_UNIT = "iRODS:MDElement";
+	public static final String AVU_UNIT_PREFIX = "fromTemplate:";
 }
