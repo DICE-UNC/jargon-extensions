9df09e3e52b5d8b587c03468b17ac9c1d38eb4d2
michael-conway <michael_conway@unc.edu>
2016-02-08 08:25:11 -0500

    #9 refactor for md query

--- virtual-collections/src/main/java/org/irods/jargon/vircoll/types/MetadataQueryVirtualCollectionExecutor.java
+++ virtual-collections/src/main/java/org/irods/jargon/vircoll/types/MetadataQueryVirtualCollectionExecutor.java
@@ -1,27 +1,24 @@
 package org.irods.jargon.vircoll.types;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.irods.jargon.core.connection.IRODSAccount;
-import org.irods.jargon.core.exception.JargonException;
 import org.irods.jargon.core.pub.IRODSAccessObjectFactory;
-import org.irods.jargon.core.query.CollectionAndDataObjectListingEntry;
 import org.irods.jargon.core.query.PagingAwareCollectionListing;
+import org.irods.jargon.mdquery.exception.MetadataQueryException;
 import org.irods.jargon.mdquery.service.MetadataQueryService;
+import org.irods.jargon.mdquery.service.MetadataQueryServiceImpl;
+import org.irods.jargon.vircoll.exception.VirtualCollectionException;
 import org.irods.jargon.vircoll.impl.VirtualCollectionExecutorImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MetadataQueryVirtualCollectionExecutor extends
 		VirtualCollectionExecutorImpl<MetadataQueryVirtualCollection> {
-	private final MetadataQueryService metadataQueryService;
 
 	static Logger log = LoggerFactory
 			.getLogger(MetadataQueryVirtualCollectionExecutor.class);
 
 	/**
-	 * Create an instance of an executor for starred folders
+	 * Create an instance of an executor for Metadata Queries
 	 * 
 	 * @param metadataQueryVirtualCollection
 	 *            {@link MetadataQueryVirtualCollection} that describes the
@@ -30,22 +27,14 @@ public class MetadataQueryVirtualCollectionExecutor extends
 	 *            {@link IRODSAccessObjectFactory} used to connect to iRODS
 	 * @param irodsAccount
 	 *            {@link IRODSAccount} with host and login information
-	 * @param metadataQueryService
-	 *            {@link MetadataQuerySerice}
 	 */
 	public MetadataQueryVirtualCollectionExecutor(
 			final MetadataQueryVirtualCollection metadataQueryVirtualCollection,
 			final IRODSAccessObjectFactory irodsAccessObjectFactory,
-			final IRODSAccount irodsAccount,
-			final MetadataQueryService metadataQueryService) {
+			final IRODSAccount irodsAccount) {
 		super(irodsAccount, irodsAccessObjectFactory,
 				metadataQueryVirtualCollection);
 
-		if (metadataQueryService == null) {
-			throw new IllegalArgumentException("null metadataQueryService");
-		}
-
-		this.metadataQueryService = metadataQueryService;
 	}
 
 	/*
@@ -55,61 +44,18 @@ public class MetadataQueryVirtualCollectionExecutor extends
 	 */
 	@Override
 	public PagingAwareCollectionListing queryAll(final int offset)
-			throws JargonException {
+			throws VirtualCollectionException {
 		log.info("queryAll()");
-
-		PagingAwareCollectionListing listing = buildInitialPagingAwareCollectionListing();
-
-		log.info("adding colls");
-		this.addAndCharacterizeCollectionListingForSplitListing(listing,
-				queryCollections(0));
-
-		log.info("adding data objects");
-		this.addAndCharacterizeDataObjectListingForSplitListing(listing,
-				queryDataObjects(0));
-
-		return listing;
-	}
-
-	private List<CollectionAndDataObjectListingEntry> queryCollections(
-			final int offset) throws JargonException {
-		log.info("queryCollections()");
-
-		List<CollectionAndDataObjectListingEntry> entries = metadataQueryService
-				.executeQuery(super.getCollection().getQueryString())
-				.getCollectionAndDataObjectListingEntries();
-
-		List<CollectionAndDataObjectListingEntry> collectionEntries = new ArrayList<CollectionAndDataObjectListingEntry>();
-
-		log.info("have entries, now format");
-
-		for (CollectionAndDataObjectListingEntry entry : entries) {
-			if (entry.isCollection()) {
-				collectionEntries.add(entry);
-			}
+		MetadataQueryService metadataQueryService = new MetadataQueryServiceImpl(
+				this.getIrodsAccessObjectFactory(), this.getIrodsAccount());
+		try {
+			return metadataQueryService.executeQuery(this.getCollection()
+					.getQueryString());
+		} catch (MetadataQueryException e) {
+			log.error("error executing query:{}", this.getCollection(), e);
+			throw new VirtualCollectionException("unable to execute query", e);
 		}
 
-		return collectionEntries;
 	}
 
-	private List<CollectionAndDataObjectListingEntry> queryDataObjects(
-			final int offset) throws JargonException {
-		log.info("queryCollections()");
-
-		List<CollectionAndDataObjectListingEntry> entries = metadataQueryService
-				.executeQuery(super.getCollection().getQueryString())
-				.getCollectionAndDataObjectListingEntries();
-
-		List<CollectionAndDataObjectListingEntry> dataObjEntries = new ArrayList<CollectionAndDataObjectListingEntry>();
-
-		log.info("have entries, now format");
-
-		for (CollectionAndDataObjectListingEntry entry : entries) {
-			if (entry.isDataObject()) {
-				dataObjEntries.add(entry);
-			}
-		}
-
-		return dataObjEntries;
-	}
 }
