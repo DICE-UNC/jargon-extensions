9242ada8d0a21a3af006cee7c035b3a00b5e9312
rskarbez <skarbez@gmail.com>
2017-02-22 11:03:00 -0500

    [#2] Fixed support for LIST_ types in Metadata Templates

--- metadata-templates/src/main/java/org/irods/jargon/metadatatemplate/JargonMetadataExporter.java
+++ metadata-templates/src/main/java/org/irods/jargon/metadatatemplate/JargonMetadataExporter.java
@@ -1,5 +1,8 @@
 package org.irods.jargon.metadatatemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.irods.jargon.core.connection.IRODSAccount;
 import org.irods.jargon.core.exception.FileNotFoundException;
 import org.irods.jargon.core.exception.JargonException;
@@ -10,6 +13,11 @@ import org.irods.jargon.core.pub.IRODSAccessObjectFactory;
 import org.irods.jargon.core.pub.domain.AvuData;
 import org.irods.jargon.core.pub.io.IRODSFile;
 import org.irods.jargon.core.pub.io.IRODSFileFactoryImpl;
+import org.irods.jargon.core.query.AVUQueryElement;
+import org.irods.jargon.core.query.JargonQueryException;
+import org.irods.jargon.core.query.MetaDataAndDomainData;
+import org.irods.jargon.core.query.QueryConditionOperators;
+import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -111,6 +119,94 @@ public class JargonMetadataExporter extends AbstractMetadataExporter {
 							+ " is neither a data object nor a collection - the JargonMetadataResolver currently only supports these types of objects");
 		}
 
+		// // Clear out "stale" metadata before adding new metadata
+		// if (irodsObject.isFile()) {
+		// List<MetaDataAndDomainData> metadataList = ((DataObjectAO) objectAO)
+		// .findMetadataValuesForDataObject(pathToObject);
+		// if (!metadataList.isEmpty()) {
+		// ((DataObjectAO) objectAO)
+		// .deleteAllAVUForDataObject(pathToObject);
+		// }
+		// } else if (irodsObject.isDirectory()) {
+		// List<MetaDataAndDomainData> metadataList;
+		// try {
+		// metadataList = ((CollectionAO) objectAO)
+		// .findMetadataValuesForCollection(pathToObject);
+		// } catch (JargonQueryException e) {
+		// log.error("query exception looking up data object:{}",
+		// pathToObject, e);
+		// throw new JargonException(e);
+		// }
+		// if (!metadataList.isEmpty()) {
+		// ((CollectionAO) objectAO).deleteAllAVUMetadata(pathToObject);
+		// }
+		// }
+		/*
+		 * ((CollectionAO) objectAO).modifyAVUMetadata(absolutePath,
+		 * currentAvuData, newAvuData);
+		 * 
+		 * ((CollectionAO)
+		 * objectAO).findMetadataValuesByMetadataQueryForCollection(avuQuery,
+		 * collectionAbsolutePath)
+		 */
+
+		/*
+		 * if (object has avu with matching attribute) overwrite else add
+		 */
+		/*
+		 * Check if the object already has an AVU with this attribute name If
+		 * not, add. If so, delete, then add.
+		 * 
+		 * Need to delete then add to account for list types. (For example,
+		 * replacing a list with a shorter list.)
+		 */
+
+		List<AVUQueryElement> queryElements = new ArrayList<AVUQueryElement>();
+		List<MetaDataAndDomainData> queryResult = null;
+
+		try {
+			queryElements.add(AVUQueryElement.instanceForValueQuery(
+					AVUQueryElement.AVUQueryPart.ATTRIBUTE,
+					QueryConditionOperators.EQUAL, element.getName()));
+
+			if (irodsObject.isFile()) {
+				queryResult = ((DataObjectAO) objectAO)
+						.findMetadataValuesForDataObjectUsingAVUQuery(
+								queryElements, pathToObject);
+			} else if (irodsObject.isDirectory()) {
+				queryResult = ((CollectionAO) objectAO)
+						.findMetadataValuesByMetadataQueryForCollection(
+								queryElements, pathToObject);
+			}
+		} catch (JargonQueryException e) {
+			log.error(
+					"Jargon query exception looking up AVUs on iRODS object:{}",
+					pathToObject, e);
+			throw new JargonException(e);
+		}
+
+		// Attribute already exists at least once on object
+		// Need to create list of AVUs for deletion
+		if (!queryResult.isEmpty()) {
+			List<AvuData> oldAvuData = new ArrayList<AvuData>();
+			for (MetaDataAndDomainData mdd : queryResult) {
+				AvuData avuData = AvuData.instance(mdd.getAvuAttribute(),
+						mdd.getAvuValue(), mdd.getAvuUnit());
+				oldAvuData.add(avuData);
+			}
+
+			// Delete AVUs
+			if (irodsObject.isFile()) {
+				((DataObjectAO) objectAO).deleteBulkAVUMetadataFromDataObject(
+						pathToObject, oldAvuData);
+			} else if (irodsObject.isDirectory()) {
+				((CollectionAO) objectAO).deleteBulkAVUMetadataFromCollection(
+						pathToObject, oldAvuData);
+			}
+		}
+
+		// Add new AVUs, if currentValue is not empty (don't create an AVU for
+		// empty input)
 		if (!element.getCurrentValue().isEmpty()) {
 			for (String s : element.getCurrentValue()) {
 				AvuData avuData = AvuData.instance(element.getName(), s,
@@ -125,6 +221,5 @@ public class JargonMetadataExporter extends AbstractMetadataExporter {
 				}
 			}
 		}
-
 	}
 }
--- metadata-templates/src/test/java/org/irods/jargon/metadatatemplate/JargonMetadataExporterTest.java
+++ metadata-templates/src/test/java/org/irods/jargon/metadatatemplate/JargonMetadataExporterTest.java
@@ -27,9 +27,11 @@ public class JargonMetadataExporterTest {
 	private static IRODSFileSystem irodsFileSystem;
 
 	private static final String TEMPLATE_FILE_NAME1 = "src/test/resources/templates/test1.mdtemplate";
+	private static final String TEMPLATE_FILE_NAME6 = "src/test/resources/templates/test6.mdtemplate";
 	private static final String TEST_FILE_NAME = "src/test/resources/testFile.txt";
 
 	private static final String TEMPLATE_NOPATH1 = "test1.mdtemplate";
+	private static final String TEMPLATE_NOPATH6 = "test6.mdtemplate";
 	private static final String TEST_FILE_NOPATH = "testFile.txt";
 
 	public static final String IRODS_TEST_SUBDIR_PATH = "JargonMetadataExporterTest";
@@ -206,7 +208,7 @@ public class JargonMetadataExporterTest {
 
 		JargonMetadataResolver resolver = new JargonMetadataResolver(
 				irodsAccount, accessObjectFactory);
-		
+
 		JargonMetadataExporter exporter = new JargonMetadataExporter(
 				accessObjectFactory, irodsAccount);
 
@@ -309,4 +311,198 @@ public class JargonMetadataExporterTest {
 		}
 	}
 
+	@Test
+	public void saveTemplateToSystemMetadataWithListInput() throws Exception {
+		String testDirName1 = "saveTemplateToSystemMetadataWithListInputDir";
+
+		String targetIrodsCollection1 = testingPropertiesHelper
+				.buildIRODSCollectionAbsolutePathFromTestProperties(
+						testingProperties, IRODS_TEST_SUBDIR_PATH + '/'
+								+ testDirName1);
+
+		IRODSAccount irodsAccount = testingPropertiesHelper
+				.buildIRODSAccountFromTestProperties(testingProperties);
+
+		IRODSAccessObjectFactory accessObjectFactory = irodsFileSystem
+				.getIRODSAccessObjectFactory();
+
+		IRODSFile targetCollectionAsFile1 = accessObjectFactory
+				.getIRODSFileFactory(irodsAccount).instanceIRODSFile(
+						targetIrodsCollection1);
+
+		targetCollectionAsFile1.mkdirs();
+
+		JargonMetadataResolver resolver = new JargonMetadataResolver(
+				irodsAccount, accessObjectFactory);
+
+		JargonMetadataExporter exporter = new JargonMetadataExporter(
+				accessObjectFactory, irodsAccount);
+
+		String mdTemplatePath1 = resolver
+				.findOrCreateMetadataTemplatesCollection(targetIrodsCollection1);
+
+		DataTransferOperations dataTransferOperations = irodsFileSystem
+				.getIRODSAccessObjectFactory().getDataTransferOperations(
+						irodsAccount);
+
+		dataTransferOperations.putOperation(TEMPLATE_FILE_NAME6,
+				mdTemplatePath1, irodsAccount.getDefaultStorageResource(),
+				null, null);
+
+		String templateFqName1 = mdTemplatePath1 + '/' + TEMPLATE_NOPATH6;
+
+		UUID uuid1 = UUID.randomUUID();
+		AvuData avuData = AvuData.instance("test6", uuid1.toString(),
+				JargonMetadataTemplateConstants.MD_TEMPLATE_UNIT);
+		accessObjectFactory.getDataObjectAO(irodsAccount).addAVUMetadata(
+				templateFqName1, avuData);
+
+		// Create a file in targetIrodsCollection1
+		dataTransferOperations.putOperation(TEST_FILE_NAME,
+				targetIrodsCollection1,
+				irodsAccount.getDefaultStorageResource(), null, null);
+		String testFileNameFQ = targetIrodsCollection1 + '/' + TEST_FILE_NOPATH;
+
+		MetadataTemplate metadataTemplate = resolver
+				.findTemplateByFqName(templateFqName1);
+		FormBasedMetadataTemplate fbmt = null;
+
+		if (metadataTemplate.getType() == TemplateTypeEnum.FORM_BASED) {
+			fbmt = (FormBasedMetadataTemplate) metadataTemplate;
+		}
+
+		for (MetadataElement me : fbmt.getElements()) {
+			if (me.getName().equalsIgnoreCase("string_list")) {
+				me.getCurrentValue().add("value1");
+				me.getCurrentValue().add("value2");
+				me.getCurrentValue().add("value3");
+				me.getCurrentValue().add("value4");
+			}
+		}
+
+		exporter.saveTemplateToSystemMetadataOnObject(fbmt, testFileNameFQ);
+
+		List<AVUQueryElement> queryElements = new ArrayList<AVUQueryElement>();
+		List<MetaDataAndDomainData> queryResult = new ArrayList<MetaDataAndDomainData>();
+
+		queryElements.add(AVUQueryElement.instanceForValueQuery(
+				AVUQueryElement.AVUQueryPart.ATTRIBUTE,
+				QueryConditionOperators.EQUAL, "string_list"));
+
+		queryResult = accessObjectFactory.getDataObjectAO(irodsAccount)
+				.findMetadataValuesForDataObjectUsingAVUQuery(queryElements,
+						testFileNameFQ);
+
+		Assert.assertTrue(
+				"saveTemplateToSystemMetadataOnObject did not create Attribute string_list",
+				!queryResult.isEmpty());
+
+		Assert.assertTrue(
+				"saveTemplateToSystemMetadataOnObject did not create 4 instances of Attribute string_list",
+				queryResult.size() == 4);
+	}
+
+	@Test
+	public void saveTemplateToSystemMetadataDeleteListAVUsOnFile()
+			throws Exception {
+		String testDirName1 = "saveTemplateToSystemMetadataDeleteListAVUsOnFileDir";
+
+		String targetIrodsCollection1 = testingPropertiesHelper
+				.buildIRODSCollectionAbsolutePathFromTestProperties(
+						testingProperties, IRODS_TEST_SUBDIR_PATH + '/'
+								+ testDirName1);
+
+		IRODSAccount irodsAccount = testingPropertiesHelper
+				.buildIRODSAccountFromTestProperties(testingProperties);
+
+		IRODSAccessObjectFactory accessObjectFactory = irodsFileSystem
+				.getIRODSAccessObjectFactory();
+
+		IRODSFile targetCollectionAsFile1 = accessObjectFactory
+				.getIRODSFileFactory(irodsAccount).instanceIRODSFile(
+						targetIrodsCollection1);
+
+		targetCollectionAsFile1.mkdirs();
+
+		JargonMetadataResolver resolver = new JargonMetadataResolver(
+				irodsAccount, accessObjectFactory);
+
+		JargonMetadataExporter exporter = new JargonMetadataExporter(
+				accessObjectFactory, irodsAccount);
+
+		String mdTemplatePath1 = resolver
+				.findOrCreateMetadataTemplatesCollection(targetIrodsCollection1);
+
+		DataTransferOperations dataTransferOperations = irodsFileSystem
+				.getIRODSAccessObjectFactory().getDataTransferOperations(
+						irodsAccount);
+
+		dataTransferOperations.putOperation(TEMPLATE_FILE_NAME6,
+				mdTemplatePath1, irodsAccount.getDefaultStorageResource(),
+				null, null);
+
+		String templateFqName1 = mdTemplatePath1 + '/' + TEMPLATE_NOPATH6;
+
+		UUID uuid1 = UUID.randomUUID();
+		AvuData avuData = AvuData.instance("test6", uuid1.toString(),
+				JargonMetadataTemplateConstants.MD_TEMPLATE_UNIT);
+		accessObjectFactory.getDataObjectAO(irodsAccount).addAVUMetadata(
+				templateFqName1, avuData);
+
+		// Create a file in targetIrodsCollection1
+		dataTransferOperations.putOperation(TEST_FILE_NAME,
+				targetIrodsCollection1,
+				irodsAccount.getDefaultStorageResource(), null, null);
+		String testFileNameFQ = targetIrodsCollection1 + '/' + TEST_FILE_NOPATH;
+
+		// Create a list AVU on the test file
+		accessObjectFactory.getDataObjectAO(irodsAccount).addAVUMetadata(
+				testFileNameFQ, AvuData.instance("string_list", "value1", ""));
+		accessObjectFactory.getDataObjectAO(irodsAccount).addAVUMetadata(
+				testFileNameFQ, AvuData.instance("string_list", "value2", ""));
+		accessObjectFactory.getDataObjectAO(irodsAccount).addAVUMetadata(
+				testFileNameFQ, AvuData.instance("string_list", "value3", ""));
+		accessObjectFactory.getDataObjectAO(irodsAccount).addAVUMetadata(
+				testFileNameFQ, AvuData.instance("string_list", "value4", ""));
+
+		MetadataTemplate metadataTemplate = resolver
+				.findTemplateByFqName(templateFqName1);
+		FormBasedMetadataTemplate fbmt = null;
+
+		if (metadataTemplate.getType() == TemplateTypeEnum.FORM_BASED) {
+			fbmt = (FormBasedMetadataTemplate) metadataTemplate;
+		}
+
+		for (MetadataElement me : fbmt.getElements()) {
+			if (me.getName().equalsIgnoreCase("string_list")) {
+				me.getCurrentValue().add("new_value");
+			}
+		}
+
+		exporter.saveTemplateToSystemMetadataOnObject(fbmt, testFileNameFQ);
+
+		List<AVUQueryElement> queryElements = new ArrayList<AVUQueryElement>();
+		List<MetaDataAndDomainData> queryResult = new ArrayList<MetaDataAndDomainData>();
+
+		queryElements.add(AVUQueryElement.instanceForValueQuery(
+				AVUQueryElement.AVUQueryPart.ATTRIBUTE,
+				QueryConditionOperators.EQUAL, "string_list"));
+
+		queryResult = accessObjectFactory.getDataObjectAO(irodsAccount)
+				.findMetadataValuesForDataObjectUsingAVUQuery(queryElements,
+						testFileNameFQ);
+
+		Assert.assertTrue(
+				"saveTemplateToSystemMetadataOnObject did not create Attribute string_list",
+				!queryResult.isEmpty());
+
+		Assert.assertTrue(
+				"saveTemplateToSystemMetadataOnObject removed original instances of Attribute string_list",
+				queryResult.size() == 1);
+		
+		for (MetaDataAndDomainData mdd : queryResult) {
+			Assert.assertEquals("string_list has wrong value",
+					"new_value", mdd.getAvuValue());
+		}
+	}
 }
