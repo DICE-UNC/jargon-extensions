9870c0c82702e95b727445f2dd62f9f73d244492
Lisa Stillwell <lisa@renci.org>
2017-02-23 10:52:47 -0500

    #17 Add pidservice handle implementation class

---
+++ pidservice-handle/src/main/java/org/irods/jargon/pidservicehandle/PidServiceHandleImpl.java
@@ -0,0 +1,22 @@
+package org.irods.jargon.pidservicehandle;
+
+import org.irods.jargon.core.exception.JargonException;
+import org.irods.jargon.core.pub.domain.DataObject;
+import org.irods.jargon.pidservice.DataObjectPID;
+import org.irods.jargon.pidservice.PidService;
+
+public class PidServiceHandleImpl implements PidService {
+
+	@Override
+	public DataObjectPID createPID(DataObjectPID pid) throws JargonException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataObject resolvePID(DataObjectPID pid) throws JargonException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
